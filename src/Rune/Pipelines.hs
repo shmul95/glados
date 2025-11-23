133c7c5dec36eb1065c39c6d33fd8713146c0ba7
Leorevoir <leo.quinzler@epitech.eu>
2025-11-23 15:47:12 +0100

    [FEAT]: cleaner pipeline

--- src/Rune/Pipelines.hs
+++ src/Rune/Pipelines.hs
@@ -1,11 +1,16 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TupleSections #-}
+
 module Rune.Pipelines (compilePipeline, interpretPipeline) where
 
 import Control.Exception (IOException, try)
+import Control.Monad ((>=>))
 import Logger (logError)
 import Rune.AST.Nodes (Program)
 import Rune.AST.Parser (parseRune)
 import Rune.AST.Printer (prettyPrint)
 import Rune.Lexer.Lexer (lexer)
+import Rune.Lexer.Tokens (Token)
 import Text.Megaparsec (errorBundlePretty)
 
 --
@@ -13,29 +18,53 @@ import Text.Megaparsec (errorBundlePretty)
 --
 
 compilePipeline :: FilePath -> FilePath -> IO ()
-compilePipeline inFile outFile = do
-  fileResult <- try (readFile inFile) :: IO (Either IOException String)
-  case fileResult of
-    Left ex -> logError $ "Failed to read input file: " ++ show ex
-    Right content -> processContent inFile content $ \ast ->
-      writeFile outFile $ prettyPrint ast
+compilePipeline inFile outFile = runPipelineAction inFile (writeFile outFile . prettyPrint)
 
 interpretPipeline :: FilePath -> IO ()
-interpretPipeline inFile = do
-  fileResult <- try (readFile inFile) :: IO (Either IOException String)
-  case fileResult of
-    Left ex -> logError $ "Failed to read input file: " ++ show ex
-    Right content -> processContent inFile content $ \ast ->
-      putStrLn $ prettyPrint ast
+interpretPipeline inFile = runPipelineAction inFile (putStrLn . prettyPrint)
 
 --
--- private
+-- private pipelines
 --
 
-processContent :: FilePath -> String -> (Program -> IO ()) -> IO ()
-processContent filename content onSuccess =
-  case lexer filename content of
-    Left err -> logError $ errorBundlePretty err
-    Right tokens -> case parseRune filename tokens of
-      Left err -> logError err
-      Right ast -> onSuccess ast
+pipeline :: (FilePath, String) -> Either String Program
+pipeline = parseLexer >=> parseAST
+
+-- >=> analyzeSemantics
+-- >=> createIR
+-- >=> optimizeIR
+-- >=> generateCode
+
+runPipeline :: FilePath -> IO (Either String Program)
+runPipeline fp = do
+  readContent <- safeRead fp
+  pure $ readContent >>= (pipeline . (fp,))
+
+runPipelineAction :: FilePath -> (Program -> IO ()) -> IO ()
+runPipelineAction inFile onSuccess =
+  runPipeline inFile >>= \case
+    Left err -> logError err
+    Right ast -> onSuccess ast
+
+--
+-- private encapsulations for error handling
+--
+
+safeRead :: FilePath -> IO (Either String String)
+safeRead fp = do
+  r <- try (readFile fp) :: IO (Either IOException String)
+  pure $ case r of
+    Left ex -> Left $ "Failed to read input file: " ++ show ex
+    Right content -> Right content
+
+parseLexer :: (FilePath, String) -> Either String (FilePath, [Token])
+parseLexer (fp, content) =
+  case lexer fp content of
+    Left err -> Left $ errorBundlePretty err
+    Right tokens -> Right (fp, tokens)
+
+parseAST :: (FilePath, [Token]) -> Either String Program
+parseAST (fp, tokens) =
+  case parseRune fp tokens of
+    Left err -> Left err
+    Right ast -> Right ast
