2c03d3201adeed1b5461d687a3011e9bdd0d5837
Mikaëla Rabines <mikaela.rabines@epitech.eu>
2025-11-25 13:46:26 +0100

    [FIX] main Rune

--- Rune/test/Spec.hs
+++ Rune/test/Spec.hs
@@ -1,18 +1,12 @@
 import Test.Tasty
 
-import ASTSpec (astTests)
-import ParserSpec (parserTests)
-import SExprSpec (sexprTests)
--- import CLISpec (cliTests)
--- import LexerSpec (lexerTests)
--- import PipelinesSpec (pipelinesTests)
+import CLISpec (cliTests)
+import LexerSpec (lexerTests)
+import PipelinesSpec (pipelinesTests)
 
 main :: IO ()
 main = defaultMain $ testGroup "All Tests"
-  [ astTests
-  , parserTests
-  , sexprTests
-  -- , cliTests
-  -- , lexerTests
-  -- , pipelinesTests
+  [cliTests
+  , lexerTests
+  , pipelinesTests
   ]
