cccf160d67101ca3ef9da74bfbd4e15ba6c5431d
gabriel-thuus <gabriel.thuus@epitech.eu>
2025-12-01 11:32:45 +0100

    [REFACTO]: Update Main.hs to be more haskell like

--- Lisp/app/Main.hs
+++ Lisp/app/Main.hs
@@ -7,46 +7,32 @@ import Executor (executeLispWithEnv, astToString)
 import Lisp.AST.AST (Environment)
 
 main :: IO ()
-main = do
-    args <- getArgs
-    case args of
-        [] -> do
-            isTerminal <- hIsTerminalDevice stdin
-            if isTerminal
-                then lispLoop []
-                else do
-                    input <- getContents
-                    processInput input
-        [filename] -> readFile filename >>= processInput
-        _ -> hPutStrLn stderr "Usage: glados [script.lisp]"
+main = getArgs >>= \case
+    [] -> hIsTerminalDevice stdin >>= \case
+        True -> lispLoop []
+        False -> getContents >>= processInput
+    [filename] -> readFile filename >>= processInput
+    _ -> hPutStrLn stderr "Usage: glados [script.lisp]"
 
 lispLoop :: Environment -> IO ()
 lispLoop env = do
     putStr "> "
     hFlush stdout
-    line <- getLine
-    case trim line of
-        "" -> lispLoop env
-        "exit" -> return ()
-        input -> do
-            let (newEnv, result) = executeLispWithEnv env input
-            case result of
-                Left err -> putStrLn err >> lispLoop newEnv
-                Right ast -> do
-                    let output = astToString ast
-                    unless (null output) (putStrLn output)
-                    lispLoop newEnv
+    getLine >>= handleInput env . trim
+
+handleInput :: Environment -> String -> IO ()
+handleInput env = \case
+    "" -> lispLoop env
+    "exit" -> return ()
+    input -> do
+        let (newEnv, result) = executeLispWithEnv env input
+        either putStrLn (putStrLn . astToString) result
+        lispLoop newEnv
 
 processInput :: String -> IO ()
 processInput input = do
     let (_, result) = executeLispWithEnv [] input
-    case result of
-        Left err -> hPutStrLn stderr err
-        Right ast -> putStrLn (astToString ast)
-
-unless :: Bool -> IO () -> IO ()
-unless True _ = return ()
-unless False action = action
+    either (hPutStrLn stderr) (putStrLn . astToString) result
 
 trim :: String -> String
 trim = f . f
