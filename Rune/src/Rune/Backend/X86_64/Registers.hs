a18163d966e28b559c14b3157c6d12a2ec4c0956
Leorevoir <leo.quinzler@epitech.eu>
2025-12-01 21:43:54 +0100

    [ADD]: caller & callee nasm reg

--- Rune/src/Rune/Backend/X86_64/Registers.hs
+++ Rune/src/Rune/Backend/X86_64/Registers.hs
@@ -1,6 +1,8 @@
 module Rune.Backend.X86_64.Registers
   ( x86_64Registers,
     x86_64ArgsRegisters,
+    x86_64CallerSavedRegisters,
+    x86_64CalleeSavedRegisters,
   )
 where
 
@@ -36,4 +38,27 @@ x86_64ArgsRegisters =
     "rcx",
     "r8",
     "r9"
+  ]
+
+x86_64CallerSavedRegisters :: [String]
+x86_64CallerSavedRegisters =
+  [ "rax",
+    "rcx",
+    "rdx",
+    "rsi",
+    "rdi",
+    "r8",
+    "r9",
+    "r10",
+    "r11"
+  ]
+
+x86_64CalleeSavedRegisters :: [String]
+x86_64CalleeSavedRegisters =
+  [ "rbx",
+    "rbp",
+    "r12",
+    "r13",
+    "r14",
+    "r15"
   ]
