a2d133b90eb101cc6a4cc988d1d4b243ab64c4e4
Leorevoir <leo.quinzler@epitech.eu>
2025-12-02 21:33:45 +0100

    [REMOVE]: bad tests

--- Rune/test/Spec.hs
+++ Rune/test/Spec.hs
@@ -4,16 +4,15 @@ import AST.NodesSpec (astNodesTests)
 import AST.ParserSpec (astParserTests)
 import AST.PrinterSpec (astPrinterTests)
 import AST.ProgramSyntaxSpec (programSyntaxTests)
+import Backend.X86_64Spec (x86_64Tests)
 import CLISpec (cliTests)
 import IR.IRNodesSpecs (irNodesTests)
 import IR.IRSpec (irTests)
 import Lexer.LexerSpec (lexerTests)
 import Lexer.TokensSpec (tokensTests)
-import LoggerSpec (loggerTests)
 import PipelinesSpec (pipelinesTests)
 import Semantics.FuncSpec (funcSemanticsTests)
 import Semantics.VarsSpec (varsSemanticsTests)
-import Backend.X86_64Spec (x86_64Tests)
 import Test.Tasty (TestTree, defaultMain, testGroup)
 
 --
@@ -41,8 +40,7 @@ coreSpecs :: TestTree
 coreSpecs =
   testGroup
     "Core Tests"
-    [ loggerTests,
-      pipelinesTests,
+    [ pipelinesTests,
       cliTests
     ]
 
@@ -84,6 +82,5 @@ backendSpecs :: TestTree
 backendSpecs =
   testGroup
     "Backend Tests"
-    [
-      x86_64Tests
+    [ x86_64Tests
     ]
