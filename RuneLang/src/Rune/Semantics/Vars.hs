ec15933d139316d7b3317acca47ebd68297efac1
Leorevoir <leo.quinzler@epitech.eu>
2025-12-21 12:06:14 +0100

    fix(semantic): foreach inner type for any[]

--- RuneLang/src/Rune/Semantics/Vars.hs
+++ RuneLang/src/Rune/Semantics/Vars.hs
@@ -206,8 +206,13 @@ verifScope vs (StmtForEach v t iter body : stmts) = do
   let s   = (fs, vs)
   e_t     <- lift $ exprType s iter
 
-  vs'     <- lift $ assignVarType vs v e_t
-  t'      <- lift $ checkMultipleType v t e_t
+  elem_t <- case e_t of
+    TypeArray inner -> pure inner
+    TypeAny -> pure TypeAny
+    _ -> lift $ Left $ printf "\n\tIteratingNonArray: cannot iterate type %s, expected array" (show e_t)
+
+  vs'     <- lift $ assignVarType vs v elem_t
+  t'      <- lift $ checkMultipleType v t elem_t
 
   iter'   <- verifExpr vs' iter
   body'   <- verifScope vs' body
