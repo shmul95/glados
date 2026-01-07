e4f839d764a7a17cef7a7c382551fdd327bb5b8e
gabriel-thuus <gabriel.thuus@epitech.eu>
2026-01-07 14:15:03 +0100

    chore: Enable optimizeIR

--- RuneLang/src/Rune/Pipelines.hs
+++ RuneLang/src/Rune/Pipelines.hs
@@ -97,7 +97,7 @@ pipeline =
   parseLexer
     >=> parseAST
     >=> verifAndGenIR
-    -- >=> optimizeIR
+    >=> optimizeIR
 
 verifAndGenIR :: Program -> Either String IRProgram
 verifAndGenIR p = do
